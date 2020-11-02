45659cafb6dfbd38c1071fd789f79ed9cef5de1c
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2020-11-03 03:05:12 +0500

    Update structure

---
+++ example/example.js
@@ -0,0 +1,20 @@
+// Simple code to show code style
+console.log("Jesse");
+
+const x = 10;
+
+let y = 5;
+y = 5 * 2;
+
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
+
+if (x === 10) {
+  console.log("x = 10");
+}
+
+setTimeout(() => {
+  y -= 5;
+}, 5000);
+
